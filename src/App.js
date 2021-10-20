849956946e054bdfc0fb2a097dacc05d2efd29c5
hamzaig <hamzaig@yahoo.com>
2021-10-20 21:46:10 +0500

    Context Api With Consumer

---
+++ src/App.js
@@ -0,0 +1,43 @@
+import React, { useEffect, useState } from 'react';
+
+import Login from './components/Login/Login';
+import Home from './components/Home/Home';
+import MainHeader from './components/MainHeader/MainHeader';
+import AuthContext from './store/auth-context';
+
+function App() {
+  // console.log("Hello");
+  const [isLoggedIn, setIsLoggedIn] = useState(false);
+
+  useEffect(() => {
+    if (localStorage.getItem("isLoggedIn") === "1") {
+      setIsLoggedIn(true);
+    }
+  }, [])
+
+  const loginHandler = (email, password) => {
+    // We should of course check email and password
+    // But it's just a dummy/ demo anyways
+    localStorage.setItem("isLoggedIn", "1");
+    setIsLoggedIn(true);
+  };
+
+  const logoutHandler = () => {
+    localStorage.removeItem("isLoggedIn");
+    setIsLoggedIn(false);
+  };
+
+  return (
+    <AuthContext.Provider value={{
+      isLoggedIn,
+    }}>
+      <MainHeader onLogout={logoutHandler} />
+      <main>
+        {!isLoggedIn && <Login onLogin={loginHandler} />}
+        {isLoggedIn && <Home onLogout={logoutHandler} />}
+      </main>
+    </AuthContext.Provider>
+  );
+}
+
+export default App;
---
+++ src/components/MainHeader/Navigation.js
@@ -0,0 +1,36 @@
+import React from 'react';
+import AuthContext from '../../store/auth-context';
+
+import classes from './Navigation.module.css';
+
+const Navigation = (props) => {
+  return (
+    <AuthContext.Consumer>
+      {(contextData) => {
+        return (
+          <nav className={classes.nav}>
+            <ul>
+              {contextData.isLoggedIn && (
+                <li>
+                  <a href="/">Users</a>
+                </li>
+              )}
+              {contextData.isLoggedIn && (
+                <li>
+                  <a href="/">Admin</a>
+                </li>
+              )}
+              {contextData.isLoggedIn && (
+                <li>
+                  <button onClick={props.onLogout}>Logout</button>
+                </li>
+              )}
+            </ul>
+          </nav>
+        );
+      }}
+    </AuthContext.Consumer>
+  );
+};
+
+export default Navigation;
